1bc01997313283309ecf9b96052263bc448e399c
Jiaomubaobao233 <287668875@qq.com>
2025-02-02 14:19:49 +0800

    subfolder

--- Shelf.py
+++ Shelf.py
@@ -19,7 +19,7 @@ class Shelf():
         if not os.path.isabs(root_folder_path):
             self.root_folder_path = pathlib.Path(os.path.realpath(__file__)).parent.absolute()
         self.catalog_folder = catalog_folder
-        self.catalog_path = os.path.join(self.root_folder_path, catalog_folder)
+        # self.catalog_path = os.path.join(self.root_folder_path, catalog_folder)
         self.html_main_local = ""
         self.html_main_remote = ""
 
@@ -63,26 +63,33 @@ class Shelf():
             self.html_main_local += s
             self.html_main_remote += s
 
-        count = 0
-        for category in os.listdir(self.catalog_path):
-            category_path = os.path.join(self.catalog_path, category)
-            if not os.path.isfile(category_path):
-                for pic in os.listdir(category_path):
-                    pic_path = os.path.join(category_path, pic)
-                    if os.path.isfile(pic_path):
-                        img = Image.open(pic_path)
-                        width, height = img.size
-                        height = min(3000, height)
-                        count += 1
-                        write_string_to_htmls('<div class="image"><img src="')
-                        self.html_main_local += pic_path
-                        self.html_main_remote += self.remote_folder + "/" + self.catalog_folder + "/" + category + "/" + pic
-                        write_string_to_htmls('" height={}>'.format(int(height ** 0.8)))
-                        if count_every > 0 and count % count_every == 0:
-                            write_string_to_htmls("<div class='caption-left'>{}</div>".format(count))
-                        write_string_to_htmls("</div>")
+        self.count = 0
 
-        write_string_to_htmls("<!-- {} books. -->".format(count))
+        def collect_folder(current_folder):
+            current_path = os.path.join(self.root_folder_path, current_folder)
+            for item in os.listdir(current_path):
+                item_path = os.path.join(current_path, item)
+                if os.path.isfile(item_path):
+                    try:
+                        with Image.open(item_path) as img:
+                            img.verify()  # Verify if it's an image
+                            width, height = img.size
+                            height = min(3000, height)
+                            self.count += 1
+                            write_string_to_htmls('<div class="image"><img src="')
+                            self.html_main_local += item_path
+                            self.html_main_remote += self.remote_folder + "/" + current_folder + "/" + item
+                            write_string_to_htmls('" height={}>'.format(int(height ** 0.8)))
+                            if count_every > 0 and self.count % count_every == 0:
+                                write_string_to_htmls("<div class='caption-left'>{}</div>".format(self.count))
+                            write_string_to_htmls("</div>")
+                    except:
+                        pass
+                else:
+                    collect_folder(os.path.join(current_folder, item))
+
+        collect_folder(self.catalog_folder)
+        write_string_to_htmls("<!-- {} books. -->".format(self.count))
 
         with codecs.open(os.path.join(self.root_folder_path, "Shelf.html"), 'w',
                          "utf-8") as html_file:
@@ -92,4 +99,4 @@ class Shelf():
                          "utf-8") as html_file:
             html_file.write(html_begin + self.html_main_remote + html_end)
 
-        print(count, "books.")
+        print(self.count, "books.")
